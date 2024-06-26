9c66a790a938e6c9676d31705deafba121802172
David Hildebrand <david@hildebrand.name>
2024-06-25 21:30:54 -0400

    added PSTH-like plot of population mean responses

--- analysis_for_images.py
+++ analysis_for_images.py
@@ -1023,6 +1023,39 @@ if n_conds != conditions.shape[0]:
     del u, c, mult
 
 
+# % Plot population mean response (similar to PSTH)
+
+fr = md['framerate']
+fig_psth = plt.figure()
+fig_psth.suptitle('mean population response (all ROIs) by category')
+axes = fig_psth.subplots(nrows=n_metrics, ncols=1)
+for m, met in enumerate(metrics):
+    ymin = np.min(np.array([np.mean(data[data['cat'] == categories[cat]][met], axis=(0, 1, 2)) for cat in range(n_cats)]))
+    ymax = np.max(np.array([np.mean(data[data['cat'] == categories[cat]][met], axis=(0, 1, 2)) for cat in range(n_cats)]))
+    ax = axes[m]
+    ax.set_ylabel(metric_labels[met])
+    ax.tick_params(axis='both', which='major', labelsize=8)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    xticks = [x * fr for x in range(np.ceil(dur_trial).astype('int') + 1)]
+    xticklabels = ['' if not np.isclose(xt, dur_isi * fr) and not np.isclose(xt, (dur_isi + dur_stim) * fr)
+                   else '{}'.format(np.round(xt / fr).astype('int')) for xt in xticks]
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels)
+    ax.axvspan(dur_isi * fr, (dur_isi + dur_stim) * fr, color='0.9', zorder=0)
+    ax.set_xlim((0, np.ceil(dur_trial) * fr))
+    ax.set_ylim((ymin - 0.1 * np.abs(ymin), ymax + 0.1 * np.abs(ymax)))
+    for cat in range(n_cats):
+        n_cnd_in_cat = data[data['cat'] == categories[cat]]['cond'].shape[0]
+        Fmean = np.mean(data[data['cat'] == categories[cat]][met], axis=(0, 1, 2))
+        Fsem = np.std(data[data['cat'] == categories[cat]][met], axis=(0, 1, 2)) / np.sqrt(n_ROIs)
+        ax.plot(range(n_samp_trial), Fmean, label=template_labels[categories[cat]], zorder=3)
+        ax.fill_between(range(n_samp_trial), Fmean - Fsem, Fmean + Fsem, alpha=0.2, zorder=2)
+    ax.legend(fontsize=4, ncol=len(ax.get_lines()), frameon=False, loc=(.02, .85))
+plt.show()
+del fr, xticks, xticklabels
+
+
 # % Calculate tuning properties for each ROI (i.e. compute face selectivity index)
 # ? ? ? and find preferred face(s)?
 #
