f4d12f84d20d04b58a68d42189a4750d6b8f5a8b
David Hildebrand <david@hildebrand.name>
2024-07-31 14:31:55 -0400

    process all images individually

--- torch_test.py
+++ torch_test.py
@@ -5,12 +5,18 @@ Created on Tue Jul 23 12:45:17 2024
 @author: DavidH
 """
 
+# Insights from ...
+# https://pytorch.org/hub/pytorch_vision_alexnet/
+# https://blog.paperspace.com/alexnet-pytorch/
+# https://www.kaggle.com/code/vortexkol/alexnet-cnn-architecture-on-tensorflow-beginner
+
 import filetype
+import numpy as np
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 import socket
 import torch
-
+from torchvision import transforms
 
 # Create hook for getting intermediate layer output
 # based on https://discuss.pytorch.org/t/how-can-i-extract-intermediate-layer-output-from-loaded-cnn-model/77301/3
@@ -24,6 +30,8 @@ def get_activation(name):
 
 # based on https://stackoverflow.com/questions/43864101/python-pil-check-if-image-is-transparent
 def has_transparency(image):
+    if image.mode == 'LA':
+        image = image.convert('RGBA')
     if image.info.get('transparency', None) is not None:
         return True
     if image.mode == 'P':
@@ -38,11 +46,12 @@ def has_transparency(image):
 
 
 system_name = socket.gethostname()
-if 'Galactica' in system_name:
+if 'Galactica' in system_name or 'galactica' in system_name:
     base_path = r'/Users/davidh/Data/Freiwald/'
     stim_path = r'/Users/davidh/Sync/Freiwald/MarmoScope/Stimulus/Sets'
     collated_stim_path = os.path.join(base_path, 'stims')
-    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+    device = 'cpu'
+    # device = 'mps' if torch.backends.mps.is_available() else 'cpu'
 elif 'Obsidian' in system_name:
     base_path = r'F:\Data'
     stim_path = r'F:\Sync\Freiwald\MarmoScope\Stimulus\Sets'
@@ -59,68 +68,112 @@ else:
     collated_stim_path = None
     device = 'cpu'
 
+# Read ImageNet categories
+with open(os.path.join(base_path, 'imagenet_classes.txt'), 'r') as f:
+    categories = [s.strip() for s in f.readlines()]
+
+model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
+fc6 = model.classifier[1]  # output from 'fc6' (1): Linear(in_features=9216, out_features=4096, bias=True)
+fc6.register_forward_hook(get_activation('fc6'))
+fc6relu = model.classifier[2]
+fc6relu.register_forward_hook(get_activation('fc6relu'))
+fc7 = model.classifier[4]  # output from 'fc6' (1): Linear(in_features=9216, out_features=4096, bias=True)
+fc7.register_forward_hook(get_activation('fc7'))
+fc7relu = model.classifier[5]
+fc7relu.register_forward_hook(get_activation('fc7relu'))
+
 image_files = [f for f in os.listdir(collated_stim_path)
                if os.path.isfile(os.path.join(collated_stim_path, f))
                and filetype.is_image(os.path.join(collated_stim_path, f))]
+n_images = len(image_files)
 
-for imf in image_files:
-    filename = os.path.join(collated_stim_path, imf)
-    with Image.open(filename) as img:
-        if has_transparency(img):
-            print(f'{imf} has transparency')
+background_intensity = 128
+fc6_features = np.full([n_images, 4096], np.nan)
+fc6relu_features = np.full([n_images, 4096], np.nan)
+fc7_features = np.full([n_images, 4096], np.nan)
+fc7relu_features = np.full([n_images, 4096], np.nan)
+for i_im, image_file in enumerate(image_files):
+    filename = os.path.join(collated_stim_path, image_file)
+    with Image.open(filename) as image:
+        if has_transparency(image):
+            # print(f'{image_file} has transparency')
+            image = image.convert('RGBA')
+            background = Image.new('RGBA', image.size, (background_intensity,) * 3)
+            composite = Image.alpha_composite(background, image)
+            image = composite.convert('RGB')
         else:
-            print(f'{imf} does not have transparency')
-        # img.show()
+            # print(f'{image_file} does not have transparency')
+            image = image.convert('RGB')
 
+        if image.size[0] != image.size[1]:
+            # print(f'{image_file} is not square')
+            image = ImageOps.pad(image, size=(np.max(image.size[:2]),) * 2, color=(background_intensity,) * 3)
 
-model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
-# model.eval()
-fc6 = model.classifier[1]  # want output from 'fc6' (1): Linear(in_features=9216, out_features=4096, bias=True)
-fc6.register_forward_hook(get_activation('fc6'))
+    preprocess = transforms.Compose([
+        transforms.Resize(224),
+        # transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    tensor = preprocess(image)
+    input_batch = tensor.unsqueeze(0)  # create a mini-batch as expected by the model
 
+    input_batch = input_batch.to(device)
+    model.to(device)
 
-for imf in image_files:
+    with torch.no_grad():
+        output = model(input_batch)
 
+    fc6_features[i_im, :] = activation['fc6'].cpu().numpy().squeeze()
+    fc6relu_features[i_im, :] = activation['fc6relu'].cpu().numpy().squeeze()
+    fc7_features[i_im, :] = activation['fc7'].cpu().numpy().squeeze()
+    fc7relu_features[i_im, :] = activation['fc7relu'].cpu().numpy().squeeze()
 
-from PIL import Image
-from torchvision import transforms
-input_image = Image.open(filename).convert('RGBA')
-newbg = Image.new('RGBA', input_image.size, (128, 128, 128))
-ac = Image.alpha_composite(newbg, input_image)
-input_image = ac.convert('RGB')
-preprocess = transforms.Compose([
-    # transforms.Resize(256),
-    # transforms.Resize(224),
-    # transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-input_tensor = preprocess(input_image)
-input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+    # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+    top_prob, top_catid = torch.topk(probabilities, 1)
+    print(image_file, categories[top_catid], top_prob.item())
+
+# from PIL import Image
+# from torchvision import transforms
+# input_image = Image.open(filename).convert('RGBA')
+# newbg = Image.new('RGBA', input_image.size, (128, 128, 128))
+# ac = Image.alpha_composite(newbg, input_image)
+# input_image = ac.convert('RGB')
+# preprocess = transforms.Compose([
+#     # transforms.Resize(256),
+#     # transforms.Resize(224),
+#     # transforms.CenterCrop(224),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+# ])
+# input_tensor = preprocess(input_image)
+# input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
 
 # transforms.functional.to_pil_image(input_tensor)
 
-
-# move the input and model to GPU for speed if available
-if torch.cuda.is_available():
-    input_batch = input_batch.to('cuda')
-    model.to('cuda')
-
-with torch.no_grad():
-    output = model(input_batch)
-# Tensor of shape 1000, with confidence scores over ImageNet's 1000 classes
-print(output[0])
-# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-probabilities = torch.nn.functional.softmax(output[0], dim=0)
-print(probabilities)
+#
+# # move the input and model to GPU for speed if available
+# if torch.cuda.is_available():
+#     input_batch = input_batch.to('cuda')
+#     model.to('cuda')
+#
+# with torch.no_grad():
+#     output = model(input_batch)
+# # Tensor of shape 1000, with confidence scores over ImageNet's 1000 classes
+# print(output[0])
+# # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+# probabilities = torch.nn.functional.softmax(output[0], dim=0)
+# print(probabilities)
 
 # # Download ImageNet labels
 # !wget https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt
 
-# Read the categories
-with open(r"F:\Data\stimuli\imagenet_classes.txt", "r") as f:
-    categories = [s.strip() for s in f.readlines()]
-# Show top categories per image
-top5_prob, top5_catid = torch.topk(probabilities, 5)
-for i in range(top5_prob.size(0)):
-    print(categories[top5_catid[i]], top5_prob[i].item())
+# # Read the categories
+# with open(r"F:\Data\stimuli\imagenet_classes.txt", "r") as f:
+#     categories = [s.strip() for s in f.readlines()]
+# # Show top categories per image
+# top5_prob, top5_catid = torch.topk(probabilities, 5)
+# for i in range(top5_prob.size(0)):
+#     print(categories[top5_catid[i]], top5_prob[i].item())
