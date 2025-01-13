a73f46d0a9205017db389fcc5c952052c890849e
David Hildebrand <david@hildebrand.name>
2025-01-13 11:19:54 -0500

    added file checksum hash functions

--- fileio.py
+++ fileio.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 
 import argparse
+# import hashlib
 import magic
 import os
+
+
+# def calculate_sha256_hash(file_path):
+#     hash_sha256 = hashlib.sha256()
+#     with open(file_path, 'rb') as f:
+#         for chunk in iter(lambda: f.read(4096), b''):
+#             hash_sha256.update(chunk)
+#     return hash_sha256.hexdigest()
+#
+#
+# def calculate_md5_hash(file_path):
+#     hash_md5 = hashlib.md5()
+#     with open(file_path, 'rb') as f:
+#         for chunk in iter(lambda: f.read(4096), b''):
+#             hash_md5.update(chunk)
+#     return hash_md5.hexdigest()
+#
+#
+# def checksum_file(file_path):
+#     h_sha256 = hashlib.sha256()
+#     h_md5 = hashlib.md5()
+#     with open(file_path, 'rb') as f:
+#         for chunk in iter(lambda: f.read(4096), b''):
+#             h_sha256.update(chunk)
+#             h_md5.update(chunk)
+#     return h_sha256.hexdigest(), h_md5.hexdigest()
+
 
 def is_tiff(filepath: str) -> bool:
     allowed_types = ['image/tiff', 'image/tif']
