3c418e43ca92b36237ec4cf70ede303c7d682b96
David Hildebrand <david@hildebrand.name>
2025-03-22 10:55:34 -0400

    initial ANOVA for responsiveness

--- analysis_for_images.py
+++ analysis_for_images.py
@@ -44,6 +44,7 @@ threshold_dprime = 0.2
 # """
 threshold_fsi = 1 / 3
 
+threshold_alpha = 0.05
 threshold_cellprob = 0.0
 threshold_Zscore = 0.5
 
@@ -3237,33 +3238,23 @@ if saving:
 # %% Playground for ANOVA testing and ZETA testing
 
 
-# for given ROI, bins containing all stim values across all trials
+m = 'Fzsc'
 
-# conds, rois, reps, frames
+p_anova = np.full((n_ROIs), np.nan)
 
-r = 0
-
-reshape_test = data[m][:, r, :, idx_stim].swapaxes(0, 1).reshape(n_conds, n_samp_stim * n_reps)
-test = np.full((n_conds, n_samp_stim * n_reps), np.nan)
-for c in range(n_conds):
-    test[c,:] = np.ravel(data[m][c, r, :, idx_stim])
-
-np.allclose(np.nanmean(reshape_test, axis=1), np.nanmean(test, axis=1))
-
-    
-reshape_test2 = data[m][:, :, :, idx_stim].swapaxes(0, 1).reshape(n_ROIs, n_conds, n_samp_stim * n_reps)  # FAIL
-reshape_test2 = data[m][:, :, :, idx_stim].reshape(n_ROIs, n_conds, n_reps * n_samp_stim)  # FAIL
-reshape_test2 = np.vstack(np.moveaxis(data[m][:, :, :, idx_stim], [0, 1], [2, 3])).swapaxes(0, 2)
-test2 = np.full((n_ROIs, n_conds, n_samp_stim * n_reps), np.nan)
 for r in range(n_ROIs):
-    for c in range(n_conds):
-        # test2[r, c,:] = np.ravel(data[m][c, r, :, idx_stim])
-        test2[r, c,:] = data[m][c, r, :, idx_stim].flatten()
+    _, p_anova[r] = f_oneway(*[data[m][c, r, :, idx_stim].flatten() for c in range(n_conds)], nan_policy='omit')
 
+samp_stim_i = stimlog['acqfr_stim_i'].values
 
-np.allclose(np.nanmean(reshape_test2, axis=(0,1)), np.nanmean(test2, axis=(0,1)))
+# import zetapy
 
-above_threshold = np.where(np.abs(dprime[m]) >= threshold_dprime)[0]
+# p_zeta = np.array([zetapy.zetatstest(np.arange(n_frames).astype(float), Fzsc_raw[r], samp_stim_i.astype(float))[0] for r in range(n_ROIs)])
+
+# p.sum(p_anova < threshold_alpha)
+# np.sum(p_zeta < threshold_alpha)
+
+# np.where((np.abs(dprime[m]) >= threshold_dprime) & (p_anova < threshold_alpha))[0]
 
 
 # %% Playground for stimulus space testing
